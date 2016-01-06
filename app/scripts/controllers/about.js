886ead7d8fc5a9023d3a07d6e362bf88659d7bc4
Yefry Calderón <yefry.calderon@udea.edu.co>
2016-01-05 22:57:53 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanExampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanExampleApp
+ */
+angular.module('yeomanExampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanExampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanExampleApp
+ */
+angular.module('yeomanExampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
